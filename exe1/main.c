a45eaecf095ae88fbde69566a3cd2136cf4a94fc
brunozalc <brun0zalc@hotmail.com>
2024-03-20 18:07:44 +0000

    done: exercício 1

--- exe1/main.c
+++ exe1/main.c
@@ -1,17 +1,18 @@
 #include <FreeRTOS.h>
-#include <task.h>
-#include <semphr.h>
 #include <queue.h>
+#include <semphr.h>
+#include <task.h>
 
 #include "pico/stdlib.h"
 #include <stdio.h>
 
-#include "hardware/gpio.h"
 #include "hardware/adc.h"
+#include "hardware/gpio.h"
 
 void adc_1_task(void *p) {
     adc_init();
     adc_gpio_init(27);
+    adc_gpio_init(26);
 
     // 12-bit conversion, assume max value == ADC_VREF == 3.3 V
     const float conversion_factor = 3.3f / (1 << 12);
@@ -22,9 +23,9 @@ void adc_1_task(void *p) {
         result = adc_read();
         printf("voltage 1: %f V\n", result * conversion_factor);
 
-        // CÓDIGO AQUI
-
-
+        adc_select_input(0); // Select ADC input 0 (GPIO26)
+        result = adc_read();
+        printf("voltage 2: %f V\n", result * conversion_factor);
 
         vTaskDelay(pdMS_TO_TICKS(200));
     }
