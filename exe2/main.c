da75c54d0689c44044c0c4fa035ea67f192c2567
brunozalc <brun0zalc@hotmail.com>
2024-03-20 18:13:29 +0000

    done: exercício 2

--- exe2/main.c
+++ exe2/main.c
@@ -1,15 +1,16 @@
 #include <FreeRTOS.h>
-#include <task.h>
-#include <semphr.h>
 #include <queue.h>
+#include <semphr.h>
+#include <task.h>
 
+#include "hardware/pwm.h"
 #include "pico/stdlib.h"
 #include <stdio.h>
-#include "hardware/pwm.h"
 
 const int PWM_0_PIN = 4;
+const int PWM_1_PIN = 6;
 
-void led_task(void *p) {
+void red_led_task(void *p) {
     gpio_set_function(PWM_0_PIN, GPIO_FUNC_PWM);
     uint slice_num = pwm_gpio_to_slice_num(PWM_0_PIN);
     pwm_set_clkdiv(slice_num, 125);
@@ -21,11 +22,24 @@ void led_task(void *p) {
     }
 }
 
+void green_led_task(void *p) {
+    gpio_set_function(PWM_1_PIN, GPIO_FUNC_PWM);
+    uint slice_num = pwm_gpio_to_slice_num(PWM_1_PIN);
+    pwm_set_clkdiv(slice_num, 125);
+    pwm_set_wrap(slice_num, 100);
+    pwm_set_chan_level(slice_num, PWM_CHAN_A, 20);
+    pwm_set_enabled(slice_num, true);
+
+    while (true) {
+    }
+}
+
 int main() {
     stdio_init_all();
     printf("Start RTOS \n");
 
-    xTaskCreate(led_task, "LED_Task 1", 256, NULL, 1, NULL);
+    xTaskCreate(red_led_task, "LED_Task 1", 256, NULL, 1, NULL);
+    xTaskCreate(green_led_task, "LED_Task 2", 256, NULL, 1, NULL);
 
     vTaskStartScheduler();
 
